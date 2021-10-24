1767b69943a89bc15eece534c2950db7dc70770f
Valentin Fedyakov <dagot32167@gmail.com>
2021-10-24 22:04:42 +0300

    update query

--- packages/server/api/getSetForTImeline.js
+++ packages/server/api/getSetForTImeline.js
@@ -2,4 +2,8 @@ const db = require("./db");
 
 module.exports = () => db
     .promise()
-    .query(`SELECT DISTINCT report_dt FROM production.station_metrics ORDER BY report_dt DESC LIMIT 432`)
+    .query(`SELECT DISTINCT report_dt 
+                FROM production.station_metrics
+                WHERE MINUTE(report_dt) IN (0, 20,40)
+                ORDER BY report_dt DESC 
+                LIMIT 216`)
