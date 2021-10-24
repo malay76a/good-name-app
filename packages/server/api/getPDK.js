2592fc04c462d86878dd93818eb874c2e002ac8a
Valentin Fedyakov <dagot32167@gmail.com>
2021-10-24 16:47:29 +0300

    add pdk

---
+++ packages/server/api/getPDK.js
@@ -0,0 +1,6 @@
+const db = require("./db");
+
+module.exports = () => db
+    .promise()
+    .query(`SELECT * 
+                FROM production.pdk`)
--- packages/server/index.js
+++ packages/server/index.js
@@ -6,6 +6,7 @@ const stantions = require('./routes/stantions');
 const indicators = require('./routes/indicators');
 const data = require('./routes/data');
 const timeline = require('./routes/timeline');
+const pdk = require('./routes/pdk');
 
 const port = process.env.PORT || 3000;
 
@@ -15,6 +16,7 @@ app.get('/indicators', indicators);
 app.get('/stantions', stantions);
 app.get('/timeline', timeline);
 app.get('/data', data);
+app.get('/pdk', pdk);
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
